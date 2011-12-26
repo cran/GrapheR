2ee918378b406b64dd98eb9cd74077d0ed8dbae0
Unknown author <unknown@unknown>
2011-12-26 00:00:00 +0000

    version 1.9-71

--- R/code.ask.R
+++ R/code.ask.R
@@ -3,7 +3,7 @@ function() {
   question<-tkmessageBox(message=Env$voc[240,1],icon="info",type="yesno")
   if (tclvalue(question)=="yes") {
     Env$l.code$save<-TRUE
-    Env$l.code$folder<-choose.dir()
+    Env$l.code$folder<-tk_choose.dir()
     code.open()
   } else {
     Env$l.code$save<-FALSE
--- R/zzzz.R
+++ R/zzzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(lib,pkg) {
-  packageStartupMessage("\n*** GrapheR v 1.9-70 ***\n\nAvailable languages: English, French, Spanish.\nUse run.GrapheR() to launch or re-launch the interface.\n")
+  packageStartupMessage("\n*** GrapheR v 1.9-71 ***\n\nAvailable languages: English, French, Spanish.\nUse run.GrapheR() to launch or re-launch the interface.\n")
 }
